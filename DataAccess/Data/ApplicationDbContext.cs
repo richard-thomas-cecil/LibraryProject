75174691a7c3058eb80415a905283ac7ca418a0a
richard-thomas-cecil <54124382+richard-thomas-cecil@users.noreply.github.com>
2020-12-14 10:36:23 -0500

    Add project files.

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
